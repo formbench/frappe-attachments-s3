0a1f7a53cbad25c6f4f9b226b95fb48756006ebe
Pruthvi Patel <pruthvipatel145@gmail.com>
2022-02-08 18:58:32 +0530

    chore: rename settings doctype

--- frappe_s3_attachment/frappe_s3_attachment/doctype/s3_file_attachment_settings/test_s3_file_attachment_settings.py
+++ frappe_s3_attachment/frappe_s3_attachment/doctype/s3_file_attachment_settings/test_s3_file_attachment_settings.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 import frappe
 import unittest
 
-class TestS3FileAttachment(unittest.TestCase):
+class TestS3FileAttachmentSettings(unittest.TestCase):
 	pass
