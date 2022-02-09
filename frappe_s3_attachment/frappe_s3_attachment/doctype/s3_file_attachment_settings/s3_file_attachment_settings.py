73cfbbd9f483ffdea1b0415e2751f4f2ecff1a84
Pruthvi Patel <pruthvipatel145@gmail.com>
2022-02-09 17:03:23 +0530

    fix: changes in `S3 File Attachment Settings`

--- frappe_s3_attachment/frappe_s3_attachment/doctype/s3_file_attachment_settings/s3_file_attachment_settings.py
+++ frappe_s3_attachment/frappe_s3_attachment/doctype/s3_file_attachment_settings/s3_file_attachment_settings.py
@@ -3,8 +3,32 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 from frappe.model.document import Document
 
+
 class S3FileAttachmentSettings(Document):
-	pass
+    def is_enabled(self):
+        return bool(self.enable_s3_file_attachment)
+
+    def validate(self):
+        if self.is_enabled():
+            setup_custom_fields()
+
+
+def setup_custom_fields():
+    custom_fields = {
+        "File": [
+            dict(
+                fieldname="s3_file_key",
+                label="S3 File Key",
+                fieldtype="Data",
+                insert_after="content_hash",
+                read_only=1,
+                print_hide=1,
+            )
+        ],
+    }
+
+    create_custom_fields(custom_fields)
